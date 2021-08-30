7f07fe6c9b51adc2edf0784bc262aeed19e0fd9f
vladi feigin <vladifeigin@gmail.com>
2021-08-30 16:04:54 +0300

    initial version for model

---
+++ cnn_training_model_new.py
@@ -0,0 +1,220 @@
+import os
+import warnings
+import cv2
+import keras
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+#import keras
+from tensorflow import keras
+from PIL import Image
+#from tensorflow.keras import models, layers, optimizers
+from tf.keras import models, layers, optimizers
+from tf.keras.applications.vgg16 import VGG16
+from tf.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tf.keras.layers import Dense, Dropout, Flatten
+from tf.keras.models import Model
+from tf.keras.preprocessing import image as image_utils
+from tf.keras.preprocessing.image import ImageDataGenerator
+from tf.keras.utils import to_categorical
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+gestures = {'L_': 'L',
+           'fi': 'Fist',
+           'C_': 'C',
+           'ok': 'Okay',
+           'pe': 'Peace',
+           'pa': 'Palm'
+            }
+
+gestures_map = {'Fist' : 0,
+                'L': 1,
+                'Okay': 2,
+                'Palm': 3,
+                'Peace': 4
+                }
+
+def process_image(path):
+    img = Image.open(path)
+    img = img.resize((224, 224))
+    img = np.array(img)
+    return img
+
+def process_data(X_data, y_data):
+    X_data = np.array(X_data, dtype = 'float32')
+    if rgb:
+        pass
+    else:
+        X_data = np.stack((X_data,)*3, axis=-1)
+    X_data /= 255
+    y_data = np.array(y_data)
+    y_data = to_categorical(y_data)
+    return X_data, y_data
+
+def walk_file_tree(relative_path):
+    X_data = []
+    y_data = []
+    for directory, subdirectories, files in os.walk(relative_path):
+        for file in files:
+            if not file.startswith('.') and (not file.startswith('C_')):
+                path = os.path.join(directory, file)
+                gesture_name = gestures[file[0:2]]
+                y_data.append(gestures_map[gesture_name])
+                X_data.append(process_image(path))
+
+            else:
+                continue
+
+    X_data, y_data = process_data(X_data, y_data)
+    return X_data, y_data
+
+class Data(object):
+    def __init__(self):
+        self.X_data = []
+        self.y_data = []
+
+    def get_data(self):
+        return self.X_data, self.y_data
+relative_path = '/home/ec2-user/builders/data_1/silhouettes/'
+rgb = False
+
+# # This method processes the data
+X_data, y_data = walk_file_tree(relative_path)
+print ("X_data type:", type(X_data))
+print ("y_data type:", type(y_data))
+
+
+# Can also optionally use a class to get this data, in order to keep it separate from Drawing data
+silhouette = Data()
+silhouette.X_data, silhouette.y_data = walk_file_tree(relative_path)
+
+print(f'X_data shape: {X_data.shape}')
+print(f'y_data shape: {y_data.shape}')
+
+gestures_map = {3: 0,
+                2: 1,
+                7: 2,
+                1: 3,
+                'Peace': 4
+                }
+X_data = []
+y_data = []
+
+root_dir = os.fsencode('/home/ec2-user/builders/data/')
+
+for directory, subdirectories, files in os.walk(root_dir):
+    for file in files:
+        if not file.startswith(b'.'):
+            gesture_name = int(file.decode('utf8')[10:11])
+            if gesture_name in [1, 2, 3, 7]:
+                path = os.path.join(directory, file).decode('utf8')
+                #y is a lable of gesture
+                y_data.append(gestures_map[gesture_name])
+
+                img = cv2.imread(path, cv2.IMREAD_COLOR)
+                img = cv2.flip(img, 1)
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                blur = cv2.GaussianBlur(gray, (41, 41), 0)  #tuple indicates blur value
+                ret, thresh = cv2.threshold(blur, 150, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                thresh = cv2.resize(thresh, (224, 224))
+                thresh = np.array(thresh)
+                X_data.append(thresh)
+
+            else:
+                continue
+X_data, y_data=process_data(X_data, y_data)
+
+print(f'X_data shape: {X_data.shape}')
+print(f'y_data shape: {y_data.shape}')
+
+z = np.zeros((len(y_data),1))
+y_data = np.append(y_data, z, axis=1)
+
+print(f'X_data shape: {X_data.shape}')
+print(f'y_data shape: {y_data.shape}')
+
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size = 0.2, random_state=12, stratify=y_data)
+check_point_path = '/home/ec2-user/builders/model/saved_model_kaggle.hdf5'
+
+model_checkpoint = ModelCheckpoint(filepath=check_point_path, save_best_only=True)
+
+early_stopping = EarlyStopping(monitor='val_acc',
+                               min_delta=0,
+                               patience=10,
+                               verbose=1,
+                               mode='auto',
+                               restore_best_weights=True)
+imageSize = 224
+
+vgg_base = VGG16(weights='imagenet', include_top=False, input_shape=(imageSize, imageSize, 3))
+optimizer1 = optimizers.Adam()
+
+base_model = vgg_base  # Topless
+# Add top layer
+x = base_model.output
+x = Flatten()(x)
+x = Dense(128, activation='relu', name='fc1')(x)
+x = Dense(128, activation='relu', name='fc2')(x)
+x = Dense(128, activation='relu', name='fc3')(x)
+x = Dropout(0.5)(x)
+x = Dense(64, activation='relu', name='fc4')(x)
+
+predictions = Dense(5, activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Train top layers only
+for layer in base_model.layers:
+    layer.trainable = False
+
+#no in use?
+#callbacks_list = [keras.callbacks.EarlyStopping(monitor='val_acc', patience=3, verbose=1)]
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=50, batch_size=64, validation_data=(X_train, y_train), verbose=1,
+          callbacks=[early_stopping, model_checkpoint])
+
+
+# load VGG16
+# Get back the convolutional part of a VGG network trained on ImageNet
+
+imageSize = 224
+model1 = VGG16(weights='imagenet', include_top=False, input_shape=(imageSize, imageSize, 3))
+optimizer1 = optimizers.Adam()
+
+base_model = model1  # Topless
+# Add top layer
+x = base_model.output
+x = Flatten()(x)
+x = Dense(128, activation='relu', name='fc1')(x)
+x = Dense(128, activation='relu', name='fc2')(x)
+x = Dense(128, activation='relu', name='fc3')(x)
+x = Dropout(0.5)(x)
+x = Dense(64, activation='relu', name='fc4')(x)
+
+predictions = Dense(5, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Train top layer
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.Adam(),
+              metrics=['accuracy'])
+
+#not in use?
+#callbacks_list = [keras.callbacks.EarlyStopping(monitor='val_acc', patience=3, verbose=1)]
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=200, batch_size=64, validation_data=(X_train, y_train), verbose=1,
+          callbacks=[early_stopping, model_checkpoint])
+
+
+
+
