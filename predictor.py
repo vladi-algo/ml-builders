27e00884c302014059ab5f0537a7a61aeb5aa519
vladi feigin <vladifeigin@gmail.com>
2021-08-30 22:39:19 +0300

    initial predictor version

---
+++ predictor.py
@@ -0,0 +1,103 @@
+import copy
+import cv2
+import numpy as np
+from keras.models import load_model
+import time
+
+
+# parameters
+cap_region_x_begin = 0.5  # start point/total width
+cap_region_y_end = 0.8  # start point/total width
+threshold = 60  # binary threshold
+blurValue = 41  # GaussianBlur parameter
+bgSubThreshold = 50
+learningRate = 0
+
+model = load_model('/Users/vffeigin/builders/project_kojak/models/VGG_cross_validated.h5')
+
+gesture_names = {0: 'Fist',
+                 1: 'L',
+                 2: 'Okay',
+                 3: 'Palm',
+                 4: 'Peace'}
+
+
+#capture background
+bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+time.sleep(2)
+isBgCaptured = 1
+print('Background captured')
+
+
+def predict_rgb_image_vgg(image):
+    image = np.array(image, dtype='float32')
+    image /= 255
+    pred_array = model.predict(image)
+    print(f'pred_array: {pred_array}')
+    result = gesture_names[np.argmax(pred_array)]
+    print(f'Result: {result}')
+    print(max(pred_array[0]))
+    score = float("%0.2f" % (max(pred_array[0]) * 100))
+    print(result)
+    return result, score
+
+
+def remove_background(frame):
+    fgmask = bgModel.apply(frame, learningRate=learningRate)
+    kernel = np.ones((3, 3), np.uint8)
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    res = cv2.bitwise_and(frame, frame, mask=fgmask)
+    return res
+
+# Camera
+camera = cv2.VideoCapture(0)
+camera.set(10, 200)
+
+
+if camera.isOpened():
+    while True:
+    
+        ret, frame = camera.read()
+        frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+        frame = cv2.flip(frame, 1)  # flip the frame horizontally
+        cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                  (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+
+        cv2.imshow('original', frame)
+        print (type(frame))
+    
+    
+        #handle background
+        img = remove_background(frame)
+        img = img[0:int(cap_region_y_end * frame.shape[0]),
+              int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+        cv2.imshow('mask', img)
+
+        # convert the image into binary image
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+        cv2.imshow('blur', blur)
+        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        print (thresh)
+        # Add prediction and action text to thresholded image
+        # cv2.putText(thresh, f"Prediction: {prediction} ({score}%)", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+        # cv2.putText(thresh, f"Action: {action}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))  # Draw the text
+  
+        key = cv2.waitKey(10)
+  
+        # copies 1 channel BW image to all 3 RGB channels
+        target = np.stack((thresh,) * 3, axis=-1)
+        target = cv2.resize(target, (224, 224))
+        target = target.reshape(1, 224, 224, 3)
+        
+        print (target)
+        prediction, score = predict_rgb_image_vgg(target)
+    
+        print (f"prediction{prediction},score{score}")
+        
+        keyboard = cv2.waitKey(60)
+        if keyboard == 'q' or keyboard == 27:
+            break
+    
+    camera.release()
+    cv2.destroyAllWindows()
